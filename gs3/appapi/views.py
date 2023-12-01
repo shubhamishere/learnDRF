b2435c643627780fcb435d396e85388399ace441
Shubham Pandey <shubham.is.here@outlook.com>
2023-12-01 13:32:51 +0530

    1dec first commit

--- gs3/appapi/views.py
+++ gs3/appapi/views.py
@@ -6,9 +6,13 @@ from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
 import requests
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+#need to exempt csrf token requirement for now 
+@csrf_exempt
 def student_api(request):
+    #handling incoming client requests for GETting the data 
     if request.method == 'GET':
         #catch the client side request data
         incoming_json_data = request.body
@@ -29,3 +33,43 @@ def student_api(request):
         serializer = StudentSerializer(stu, many=True)
         outgoing_json_data = JSONRenderer().render(serializer.data)
         return HttpResponse(outgoing_json_data, content_type='application/json')
+
+    #handling incoming client requests for POSTing/writing/creating new data on server side
+    if request.method == 'POST':
+        #catching the client side request data
+        #client side JSON format data then--> python data type then--> complex data type then--> save in DB
+        incoming_json_data = request.body
+        stream = io.BytesIO(incoming_json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data = pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            #acknowledge the client if the data is created
+            res ={'msg': 'data created'}
+            outgoing_json_data = JSONRenderer().render(res)
+            return HttpResponse(outgoing_json_data, content_type='application/json')
+        
+        #case when data was invalid, not serialised successfuly
+        outgoing_json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(outgoing_json_data, content_type='application/json')
+        
+    #handling incoming client requests for PUTting/editing/modifying existing data on server side
+    if request.method == 'PUT':
+        incoming_json_data = request.body
+        incoming_stream = io.BytesIO(incoming_json_data)
+        #pythondata: dictionary->key value pairs
+        pythondata = JSONParser().parse(incoming_stream)
+        id = pythondata.get('id')
+        #stu fetch the existing object data for the requested student id
+        stu = Student.objects.get(id=id)
+        #stu is passed to get serialized along with the updated data
+        serializer = StudentSerializer(stu, data=pythondata, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'data updated!!'}
+            outgoing_json_data = JSONRenderer().render(res)
+            return HttpResponse(outgoing_json_data, content_type='application/json')
+
+        json_data = JSONRenderer.render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
--- gs3/clientsideapp.py
+++ gs3/clientsideapp.py
@@ -15,4 +15,50 @@ def get_data(id = None):
     print(data)
 
 #callling this fn with id 1 to get and print the data of student id 1 from the server
-get_data()
+#get_data(1)
+#get_data(2)
+#get-data()
+
+##----------------------------------------------
+#method for POSTing data
+def post_data():
+    #this dict data we will post to server
+    outgoing_data = {
+        'name': 'ravi',
+        'roll': 104,
+        'city': 'Dhanbad',
+    }
+
+    outgoing_json_data = json.dumps(outgoing_data)
+
+    #requests.post will send data to the server and receive
+    #-a response in 'r'
+    r = requests.post(url = URL, data = outgoing_json_data)
+    incoming_data = r.json()
+    print(incoming_data)
+
+#post_data()
+
+##-------------------------
+#fn for requesting to PUTting/updating data on server side
+def update_data():
+    #this dict data we will post to server
+    outgoing_data = {
+        'id': 4,
+        'name': 'abc',
+        'city': 'xyz',
+        'roll': 60,
+    }
+
+    outgoing_json_data = json.dumps(outgoing_data)
+
+    #requests.post will send data to the server and receive
+    #-a response in 'r'
+    r = requests.put(url = URL, data = outgoing_json_data)
+    incoming_data = r.json()
+    print(incoming_data)
+
+#update_data()
+
+def delete_data():
+    data = {}
